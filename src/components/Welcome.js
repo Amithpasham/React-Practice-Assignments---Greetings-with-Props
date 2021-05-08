d13e416206067a32b7f4672b1d76aacbf66fe874
Amith <amithpasham@gmail.com>
2021-05-08 17:31:01 +0530

    changee

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,8 +5,8 @@ import React, {Component, useState} from "react";
 function Welcome(props) {
 
 return (
-    <div >
-    Hey !<h1>{props.name}</h1>
+    <div>
+    Hey ! <h1>{props.name}</h1>
 <h2>Welcome to Newton School.</h2>
     </div>
     );
